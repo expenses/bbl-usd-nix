0e871ad476c6b86dadc60ac4c9cac242080ec4f3
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-21 23:22:12 +1300

    Add windows test binary

--- build.nix
+++ build.nix
@@ -9,11 +9,13 @@ stdenv.mkDerivation {
   buildInputs = [openusd-minimal vulkan-loader vulkan-sdk];
 
   cmakeFlags = [
-    "-DBBL_USD_SRC_FILE=${headers}/src/openusd-c.cpp"
+    "-DBBL_USD_SRC_DIR=${headers}/src"
   ];
 
   installPhase = ''
     mkdir -p $out/lib
-    mv libopenusd-c.a $out/lib
+    mkdir -p $out/bin
+    mv *.a $out/lib
+    mv *.exe $out/bin
   '';
 }
--- flake.nix
+++ flake.nix
@@ -11,18 +11,24 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, pkgs, system, ... }: {
+      perSystem = { config, pkgs, system, ... }:
+      let
+        openusd = openusd-minimal.packages.${system}.default.override {vulkanSupport = true; static = true;};
+        openusd-win = openusd-minimal.packages.${system}.windows.override {vulkanSupport = true;};
+      in
+      {
         packages = rec {
           default = pkgs.callPackage ./package.nix {
             babble = babble.packages.${system}.default;
-            openusd-minimal = openusd-minimal.packages.${system}.default.override {vulkanSupport = true;};
+            openusd-minimal = openusd;
             vulkan-sdk = openusd-minimal.packages.${system}.vulkan-sdk;
           };
           windows = pkgs.pkgsCross.mingwW64.callPackage ./build.nix {
             headers = default;
-            openusd-minimal = openusd-minimal.packages.${system}.windows.override {vulkanSupport = true;};
+            openusd-minimal = openusd-win;
             vulkan-sdk = openusd-minimal.packages.${system}.vulkan-sdk;
           };
+          inherit openusd openusd-win;
         };
       };
       flake = { };
