04663e22e359db44dc3be70e9007d8378fa532fa
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-21 23:28:13 +1300

    Change src

--- package.nix
+++ package.nix
@@ -3,7 +3,7 @@ stdenv.mkDerivation {
   name = "bbl-usd";
 
   src = fetchGit {
-    url = "/home/ashley/projects/bbl-usd-rs/bbl-usd";
+    url = "github:expenses/bbl-usd";
     rev = "7fc5ff666ec4ac7abce1af13b5e31edefba04e3e";
   };
 
